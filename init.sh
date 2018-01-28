36918014906c5e729789753eb3ded9e32fad373b
Aleksandr-Git <gavryukov@mail.ru>
2018-01-28 15:54:46 +0300

    15:54_28.01.18

--- init.sh
+++ init.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# создаем условную ссылку
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
+# удаляем файл конфигурации по умолчанию
 sudo rm -rf /etc/nginx/sites-enabled/default
+# перезапускаем вэб-сервер
 sudo /etc/init.d/nginx restart
