21614b31f4a2e4ca6a082519036ae1f61d5bcfbb
Aleksandr-git <gavryukov@mail.ru>
2018-10-08 14:56:30 +0300

    gunicon

--- init.sh
+++ init.sh
@@ -6,3 +6,7 @@ sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo rm -rf /etc/nginx/sites-enabled/default
 # перезапускаем вэб-сервер
 sudo /etc/init.d/nginx restart
+
+# sudo ln -sf /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
+
+# sudo /etc/init.d/gunicorn restart
