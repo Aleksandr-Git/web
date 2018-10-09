7344636492b7136d04ed013edf17566539dce3ba
Aleksandr-git <gavryukov@mail.ru>
2018-10-09 11:49:57 +0300

    gunicorn запускается но не работает

--- hello.py
+++ hello.py
@@ -1,8 +1,4 @@
-import re
-
-A = '/?a=1&a=2&b=3'
-
-result = re.findall(r'\w+=[^&]+', A)
-
-for i in result:
-    print(i)
+def app(environ, start_response):
+    data = b"Hello, World!\n"
+    start_response("200 OK", [("Content-Type", "text/plain"),"Content-Length", str(len(data))])
+    return iter([data])
