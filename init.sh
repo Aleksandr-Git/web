9976c94ed57e86e6fac9004e7edfce3931552a59
Aleksandr-git <gavryukov@mail.ru>
2018-10-09 14:25:36 +0300

    поправлен init.sh

--- init.sh
+++ init.sh
@@ -7,7 +7,9 @@ sudo rm -rf /etc/nginx/sites-enabled/default
 # перезапускаем вэб-сервер
 sudo /etc/init.d/nginx restart
 
-#sudo ln -sf /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
+sudo ln -sf /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
+
+sudo gunicorn -b 0.0.0.0:8080 hello:fun
 
 # sudo /etc/init.d/gunicorn restart
 
