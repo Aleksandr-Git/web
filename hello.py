0c0030b457b365d2554017646b673964e0a9ea7b
Aleksandr-git <gavryukov@mail.ru>
2018-10-09 15:08:26 +0300

    stepic принял, но не запускается как демон

--- hello.py
+++ hello.py
@@ -1,7 +1,10 @@
+# сервер gunicorn необходимо запускать из директории в которой лежит этот файл
+# для запуска  sudo gunicorn -b 0.0.0.0:8080 hello:fun
+
 def fun(environ, start_response):
     status = '200 OK'
     response_headers = [('Content-type', 'text/plain')]
     start_response(status, response_headers)
     body = [(i + '\n') for i in environ['QUERY_STRING'].split('&')]
-#    body = [bytes(i + '\n', 'ascii') for i in environ['QUERY_STRING'].split('&')]
+#    body = [bytes(i + '\n', 'ascii') for i in environ['QUERY_STRING'].split('&')]  # для Python 2.7
     return body
