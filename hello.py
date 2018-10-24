2dc812a3f4a6d31d2fbcf2160fc1dbad8f2cc87a
Aleksandr-Git <gavryukov@mail.ru>
2018-10-24 13:22:41 +0300

    Удалено

--- hello.py
+++
@@ -1,7 +0,0 @@
-def fun(environ, start_response):
-    status = '200 OK'
-    response_headers = [('Content-type', 'text/plain')]
-    start_response(status, response_headers)
-#    body = [(i + '\n') for i in environ['QUERY_STRING'].split('&')]
-    body = [bytes(i + '\n', 'ascii') for i in environ['QUERY_STRING'].split('&')]
-    return body
