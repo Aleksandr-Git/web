25b61fc2e4ff84a3cc9763e6a6756891d18f0e15
Aleksandr-Git <gavryukov@mail.ru>
2018-10-24 13:28:54 +0300

    добавлен init.sh

---
+++ init.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# создаем условную ссылку
+sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
+# удаляем файл конфигурации по умолчанию
+sudo rm -rf /etc/nginx/sites-enabled/default
+# перезапускаем вэб-сервер
+sudo /etc/init.d/nginx restart
+
+sudo ln -sf /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
+
+# запускать из папки в которой лежит hellop.py
+sudo gunicorn -b 0.0.0.0:8080 hello:fun &
+
+sudo gunicorn -b 0.0.0.0:8000 ask.wsgi:application &
+# sudo /etc/init.d/gunicorn restart
+
+# конец!!
